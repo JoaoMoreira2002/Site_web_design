8939aeb5f853ba30e9ec8cbaf4c6f6be9c1d9450
Joao Oliveira <joao.oliver2012@hotmail.com>
2021-11-21 22:18:15 -0300

    Add cabecalho

--- main.js
+++ main.js
@@ -1,4 +1,31 @@
+function abreModal() {
+  let containerModal = document.querySelector('.container-modal');
+  let modal = document.querySelector('.modal');
 
+  containerModal.classList.add("mostra");
+  modal.classList.add("mostra");
+}
+
+function fechaModal() {
+  let containerModal = document.querySelector('.container-modal');
+  let modal = document.querySelector('.modal');
+
+  containerModal.classList.remove("mostra");
+  modal.classList.remove("mostra");
+}
+
+let receitas = document.querySelectorAll('.nav-receitas ul li');
+
+receitas.forEach(function (e) {
+  e.addEventListener("mousemove", function () {
+      abreModal();
+  });
+});
+
+
+window.addEventListener("scroll", function(){
+  fechaModal();
+})
 
 $('.carrousel').slick({
   infinite: false,
